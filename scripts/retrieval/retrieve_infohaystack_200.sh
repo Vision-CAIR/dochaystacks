f6232829ea9450cf2a36ae138aed6a8a74c525ae
Jun Chen <jun.chen@kaust.edu.sa>
2025-01-02 19:36:22 +0800

    Update retrieve_infohaystack_200.sh

--- scripts/retrieval/retrieve_infohaystack_200.sh
+++ scripts/retrieval/retrieve_infohaystack_200.sh
@@ -8,4 +8,4 @@ IMAGE_DIR="InfoHaystack_200"
 OUTPUT_DIR="./output/infovqa_200"
 
 
-python model/VRAG_retrieval.py --dataset_file $DATASET_FILE --image_root $IMAGE_ROOT --image_dir $IMAGE_DIR --output_dir $OUTPUT_DIR
+python model/VRAG_retrieval.py --dataset_file $DATASET_FILE --image_root $IMAGE_ROOT --image_dir $IMAGE_DIR --output_dir $OUTPUT_DIR --use_question_query
