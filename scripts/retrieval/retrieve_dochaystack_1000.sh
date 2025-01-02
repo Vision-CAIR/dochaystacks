6168f76ff560f648699a119b7fa122c6e4b26c57
Jun Chen <jun.chen@kaust.edu.sa>
2025-01-02 19:35:33 +0800

    Update retrieve_dochaystack_1000.sh

--- scripts/retrieval/retrieve_dochaystack_1000.sh
+++ scripts/retrieval/retrieve_dochaystack_1000.sh
@@ -8,4 +8,4 @@ IMAGE_DIR="DocHaystack_1000"
 OUTPUT_DIR="./output/docvqa_1000"
 
 
-python model/VRAG_retrieval.py --dataset_file $DATASET_FILE --image_root $IMAGE_ROOT --image_dir $IMAGE_DIR --output_dir $OUTPUT_DIR
+python model/VRAG_retrieval.py --dataset_file $DATASET_FILE --image_root $IMAGE_ROOT --image_dir $IMAGE_DIR --output_dir $OUTPUT_DIR --use_question_query
